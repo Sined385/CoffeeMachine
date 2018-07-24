9c89730c0988cf73f952cc8b9ed5c99fe6621c50
Денис Ефремов <mac@denyska.local>
2018-07-24 23:23:47 +0300

    1 commit

---
+++ CoffeeMachine/CoffeeMachine.swift
@@ -0,0 +1,39 @@
+//
+//  CoffeeMachine.swift
+//  CoffeMachine
+//
+//  Created by mac on 24.07.2018.
+//  Copyright © 2018 SINED. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeMachine: NSObject {
+    
+    let whaterPortions: Int
+    let cofeePortions: Int
+    let milkPortions: Int
+    let donePortions: Int
+    
+    init(whaterPortions: Int, cofeePortions: Int, milkPortions: Int, donePortions: Int) {
+        self.whaterPortions = whaterPortions
+        self.cofeePortions = cofeePortions
+        self.milkPortions = milkPortions
+        self.donePortions = donePortions
+    }
+    
+    
+    func espresso() {
+        
+    }
+    
+    
+    
+   
+    
+    
+    
+    
+    
+}
+
--- CoffeeMachine/ViewController.swift
+++ CoffeeMachine/ViewController.swift
@@ -13,11 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        CoffeeMachine.init(whaterPortions: 10, cofeePortions: 10, milkPortions: 10, donePortions: 10)
+        
+        
+        
+        
+        
+        
+        
     }
 
 
